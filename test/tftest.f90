31e3b043dfca61f23583a8c4ccc2832b5eec9c72
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-09-13 15:18:17 +0200

    Add unit testing for build interface
    - update FORD input

---
+++ test/tftest.f90
@@ -0,0 +1,33 @@
+! This file is part of toml-f.
+!
+! Copyright (C) 2019-2020 Sebastian Ehlert
+!
+! Licensed under either of Apache License, Version 2.0 or MIT license
+! at your option; you may not use this file except in compliance with
+! the License.
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+
+!> Wrapper for the testsuites
+program tftester
+   use, intrinsic :: iso_fortran_env, only : error_unit
+   use tftest_testsuite, only : run_testsuite
+   use tftest_build, only : collect_build
+   implicit none
+   integer :: stat
+   character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+   write(error_unit, fmt) repeat('-', 72)
+   write(error_unit, fmt) "Testing:", "build"
+   call run_testsuite(collect_build, error_unit, stat)
+
+   if (stat > 0) then
+      write(error_unit, '(i0, 1x, a)') stat, "tests failed!"
+      error stop 1
+   end if
+
+end program tftester
