c541fce119d6a1dc16611e1fe3ff29ec071222b8
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-09-14 17:26:39 +0200

    Improve build interface for arrays

--- src/tomlf/build/array.f90
+++ src/tomlf/build/array.f90
@@ -12,10 +12,41 @@
 ! See the License for the specific language governing permissions and
 ! limitations under the License.
 
-!> Functions to build a TOML data structures
+!> Functions to build TOML array
 !
-!  The build module defines a high level interface to work with TOML data structures
+!  This build module defines a high level interface to work with TOML arrays
 !  and construct them in a convenient way.
+!
+!  The access to the array elements happens by position in the array, the indexing
+!  is one based, following the language convention of Fortran. All functions
+!  will only allow access of elements within the bounds of the array, specifying
+!  indices out-of-bounds should be save, as it only sets the status of operation.
+!  The getter functions allow access to other tables and arrays as well as
+!  convenient wrappers to retrieve value data
+!
+!  The setter functions are somewhat weaker compared to the setter functions
+!  available for TOML tables. To limit the potential havoc this routines can
+!  cause they can only access the array within its bounds. Setting a value to
+!  another value will overwrite it, while setting a value to a table or an array
+!  will fail, for safety reasons.
+!
+!  To (re)build an array appending to it is the best choice, tables and arrays
+!  should always be create by using the corresponding `add_table` and `add_array`
+!  function. While this can become cumbersome for values, the setter routines
+!  allow out-of-bound access to for the next element in an array and will indeed
+!  just append a new value to it.
+!
+!  Constructing a new array should work therefore with:
+!
+!  ```fortran
+!  type(toml_array) :: array
+!  integer :: ii, stat
+!  call new_array(array)
+!  do ii = 1, 10
+!     call set_value(array, ii, ii**2)
+!  end do
+!  print '(i0)', len(array)
+!  ```
 module tomlf_build_array
    use tomlf_build_keyval, only : get_value, set_value
    use tomlf_constants, only : tfc, tfi, tfr, tf_i1, tf_i2, tf_i4, tf_i8, &
@@ -31,17 +62,32 @@ module tomlf_build_array
    public :: get_value, set_value
 
 
-   !> Setter functions to manipulate TOML tables
-   !interface set_value
-   !end interface set_value
+   !> Setter functions to manipulate TOML arrays
+   interface set_value
+      module procedure :: set_elem_value_string
+      module procedure :: set_elem_value_real_sp
+      module procedure :: set_elem_value_real_dp
+      module procedure :: set_elem_value_int_i1
+      module procedure :: set_elem_value_int_i2
+      module procedure :: set_elem_value_int_i4
+      module procedure :: set_elem_value_int_i8
+      module procedure :: set_elem_value_bool
+   end interface set_value
 
 
-   !> Getter functions to manipulate TOML tables
+   !> Getter functions to manipulate TOML arrays
    interface get_value
       module procedure :: get_elem_table
       module procedure :: get_elem_array
       module procedure :: get_elem_keyval
       module procedure :: get_elem_value_string
+      module procedure :: get_elem_value_real_sp
+      module procedure :: get_elem_value_real_dp
+      module procedure :: get_elem_value_int_i1
+      module procedure :: get_elem_value_int_i2
+      module procedure :: get_elem_value_int_i4
+      module procedure :: get_elem_value_int_i8
+      module procedure :: get_elem_value_bool
    end interface get_value
 
 
@@ -179,6 +225,474 @@ subroutine get_elem_value_string(array, pos, val, stat)
    end if
 
 end subroutine get_elem_value_string
+
+
+!> Retrieve TOML value as single precision floating point number
+subroutine get_elem_value_real_sp(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Floating point value
+   real(tf_sp), intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_real_sp
+
+
+!> Retrieve TOML value as double precision floating point number
+subroutine get_elem_value_real_dp(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Floating point value
+   real(tf_dp), intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_real_dp
+
+
+!> Retrieve TOML value as integer value
+subroutine get_elem_value_int_i1(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i1), intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_int_i1
+
+
+!> Retrieve TOML value as integer value
+subroutine get_elem_value_int_i2(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i2), intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_int_i2
+
+
+!> Retrieve TOML value as integer value
+subroutine get_elem_value_int_i4(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i4), intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_int_i4
+
+
+!> Retrieve TOML value as integer value
+subroutine get_elem_value_int_i8(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i8), intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_int_i8
+
+
+!> Retrieve TOML value as boolean
+subroutine get_elem_value_bool(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   logical, intent(out) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (associated(ptr)) then
+      call get_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine get_elem_value_bool
+
+
+!> Retrieve TOML value as deferred-length character
+subroutine set_elem_value_string(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> String value
+   character(kind=tfc, len=*), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_string
+
+
+!> Retrieve TOML value as single precision floating point number
+subroutine set_elem_value_real_sp(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Floating point value
+   real(tf_sp), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_real_sp
+
+
+!> Retrieve TOML value as double precision floating point number
+subroutine set_elem_value_real_dp(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Floating point value
+   real(tf_dp), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_real_dp
+
+
+!> Retrieve TOML value as integer value
+subroutine set_elem_value_int_i1(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i1), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_int_i1
+
+
+!> Retrieve TOML value as integer value
+subroutine set_elem_value_int_i2(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i2), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_int_i2
+
+
+!> Retrieve TOML value as integer value
+subroutine set_elem_value_int_i4(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i4), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_int_i4
+
+
+!> Retrieve TOML value as integer value
+subroutine set_elem_value_int_i8(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Integer value
+   integer(tf_i8), intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_int_i8
+
+
+!> Retrieve TOML value as boolean value
+subroutine set_elem_value_bool(array, pos, val, stat)
+
+   !> Instance of the TOML array
+   class(toml_array), intent(inout) :: array
+
+   !> Position in the array
+   integer, intent(in) :: pos
+
+   !> Boolean value
+   logical, intent(in) :: val
+
+   !> Status of operation
+   integer, intent(out), optional :: stat
+
+   class(toml_keyval), pointer :: ptr
+
+   call get_value(array, pos, ptr, stat)
+
+   if (.not.associated(ptr)) then
+      if (pos == len(array) + 1) then
+         call add_keyval(array, ptr, stat)
+      end if
+   end if
+
+   if (associated(ptr)) then
+      call set_value(ptr, val, stat)
+   else
+      if (present(stat)) stat = toml_stat%fatal
+   end if
+
+end subroutine set_elem_value_bool
 
 
 end module tomlf_build_array
