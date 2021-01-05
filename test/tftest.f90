6b930a00c8544ec4d2e74bf250ae325588b464cd
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-01-05 09:38:52 +0100

    Add some basic unit tests for sorting

--- test/tftest.f90
+++ test/tftest.f90
@@ -16,6 +16,7 @@ program tftester
    use, intrinsic :: iso_fortran_env, only : error_unit
    use tftest_testsuite, only : run_testsuite
    use tftest_build, only : collect_build
+   use tftest_sort, only : collect_sort
    implicit none
    integer :: stat
    character(len=*), parameter :: fmt = '("#", *(1x, a))'
@@ -23,6 +24,15 @@ program tftester
    write(error_unit, fmt) repeat('-', 72)
    write(error_unit, fmt) "Testing:", "build"
    call run_testsuite(collect_build, error_unit, stat)
+
+   if (stat > 0) then
+      write(error_unit, '(i0, 1x, a)') stat, "tests failed!"
+      error stop 1
+   end if
+
+   write(error_unit, fmt) repeat('-', 72)
+   write(error_unit, fmt) "Testing:", "sort"
+   call run_testsuite(collect_sort, error_unit, stat)
 
    if (stat > 0) then
       write(error_unit, '(i0, 1x, a)') stat, "tests failed!"
