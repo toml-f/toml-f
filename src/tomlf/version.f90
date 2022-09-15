aee54c5a480d623af99828c76df0447a15ce90dc
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-09-15 10:48:58 +0200

    Bump version to 0.3.1

--- src/tomlf/version.f90
+++ src/tomlf/version.f90
@@ -21,7 +21,7 @@ module tomlf_version
 
 
    !> String representation of the TOML-Fortran version
-   character(len=*), parameter :: tomlf_version_string = "0.3.0"
+   character(len=*), parameter :: tomlf_version_string = "0.3.1"
 
    !> Major version number of the above TOML-Fortran version
    integer, parameter :: tomlf_major = 0
@@ -30,7 +30,7 @@ module tomlf_version
    integer, parameter :: tomlf_minor = 3
 
    !> Patch version number of the above TOML-Fortran version
-   integer, parameter :: tomlf_patch = 0
+   integer, parameter :: tomlf_patch = 1
 
    !> Compact numeric representation of the TOML-Fortran version
    integer, parameter :: tomlf_version_compact = &
