5e57c5ac00779ca590972dc87378c64002ec7e66
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2023-02-22 19:42:40 +0100

    Update to version 0.4.0

--- src/tomlf/version.f90
+++ src/tomlf/version.f90
@@ -21,13 +21,13 @@ module tomlf_version
 
 
    !> String representation of the TOML-Fortran version
-   character(len=*), parameter :: tomlf_version_string = "0.3.0"
+   character(len=*), parameter :: tomlf_version_string = "0.4.0"
 
    !> Major version number of the above TOML-Fortran version
    integer, parameter :: tomlf_major = 0
 
    !> Minor version number of the above TOML-Fortran version
-   integer, parameter :: tomlf_minor = 3
+   integer, parameter :: tomlf_minor = 4
 
    !> Patch version number of the above TOML-Fortran version
    integer, parameter :: tomlf_patch = 0
