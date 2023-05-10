1c68ea94a162b3584257509545865b461143219b
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2023-05-10 18:17:20 +0200

    Remove associate construct which breaks for Cray (#137)

--- src/tomlf/diagnostic.f90
+++ src/tomlf/diagnostic.f90
@@ -250,9 +250,7 @@ function render_text_with_label(input, label, color, source) result(string)
    allocate(token(0))  ! avoid compiler warning
    token = line_tokens(input)
    line = count(token%first < label%first)
-   associate(first => token%first)
-      shift = first(line) - 1
-   end associate
+   shift = token(line)%first - 1
    first = max(1, line - 1)
    last = min(size(token), line + 1)
    offset = integer_width(last)
@@ -297,9 +295,7 @@ pure function render_text_with_labels(input, label, color, source) result(string
    allocate(token(0))  ! avoid compiler warning
    token = line_tokens(input)
    line(:) = [(count(token%first <= label(it)%first), it = 1, size(label))]
-   associate(first => token%first)
-      shift(:) = first(line) - 1
-   end associate
+   shift(:) = token(line)%first - 1
    first = max(1, minval(line))
    last = min(size(token), maxval(line))
    offset = integer_width(last)
